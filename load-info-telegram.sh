13c5c885b02c8d4acd028a3b2db0b4a51c9ecad4
gramilla <iam.gramilla@gmail.com>
2020-09-21 18:44:06 +0000

    add smiles and tags

--- load-info-telegram.sh
+++ load-info-telegram.sh
@@ -4,19 +4,20 @@
 #Include telegram chat id and bot token ID here
 chat_id="225280091"
 token="1368717528:AAE88KNir5b1wRZYV87pTMwEjc6biTsVcPE"
-
+host=$HOSTNAME
+ip=($(hostname -I))
 #Temporary files to store data
-resource_usage_info=/tmp/resource_usage_info.txt
+resource_usage_info=/tmp/resource_usage_${host}.txt
 msg_caption=/tmp/telegram_msg_caption.txt
 
 #Set threshold levels for memory usage and load here. If the usage exceeds these values, the notification will be sent.
-mem_threshold=90 #Should be interger. This is in percentage
+mem_threshold=10 #Should be interger. This is in percentage
 load_threshold=$(nproc) #Should be integer. Usually total number of cores.
 
 #Telegram API to send notificaiton.
 function telegram_send
 {
-curl -s -F chat_id=$chat_id -F document=@$resource_usage_info -F caption="$caption" https://api.telegram.org/bot$token/sendDocument > /dev/null 2&>1
+curl -s -F chat_id=$chat_id -F parse_mode=markdown -F document=@$resource_usage_info -F caption="$caption" https://api.telegram.org/bot$token/sendDocument > /dev/null 2&>1
 }
 
 #Monitoring Load on the server
@@ -25,14 +26,14 @@ do
     min_load=$(cat /proc/loadavg | cut -d . -f1)
     if [ $min_load -ge $load_threshold ]
         then
-        echo -e "High CPU usage detected on $(hostname)\n$(uptime)" > $msg_caption
+        echo -e "⚠️High CPU usage detected⚠️\n🖥  $(hostname)\n🌎$ip\n⏱$(uptime)\n#CPU" > $msg_caption
         echo -e "CPU usage report from $(hostname)\nServer Time : $(date +"%d%b%Y %T")\n\n\$uptime\n$(uptime)\n\n%CPU %MEM USER\tCMD" >         $resource_usage_info
         ps -eo pcpu,pmem,user,cmd | sed '1d' | sort -nr >> $resource_usage_info
         caption=$(<$msg_caption)
         telegram_send
         rm -f $resource_usage_info
         rm -f $msg_caption
-        sleep 900 #stop executing script for 15 minutes
+        sleep 60 #stop executing script for 15 minutes
     fi
     sleep 10
 
@@ -41,14 +42,14 @@ do
     mem_usage=$(echo "$mem" | awk 'NR==2{printf "%i\n", ($3*100/$2)}')
     if [ $mem_usage -gt $mem_threshold ]
     then
-        echo -e "High Memory usage detected on $(hostname)\n$(echo $mem_usage% memory usage)" > $msg_caption
+        echo -e "⚠️High Memory usage detected⚠️\n🖥$(hostname)\n🌎$ip\n🔸$(echo $mem_usage% memory usage)\n#RAM" > $msg_caption
         echo -e "Memory consumption Report from $(hostname)\nServer Time : $(date +"%d%b%Y %T")\n\n\$free -m output\n$mem\n\n%MEM %CPU USER\tCMD" > $resource_usage_info
         ps -eo pmem,pcpu,user,cmd | sed '1d' | sort -nr >> $resource_usage_info
         caption=$(<$msg_caption)
         telegram_send
         rm -f $resource_usage_info
         rm -f $msg_caption
-        sleep 900 #stop executing script for 15 minutes
+        sleep 60 #stop executing script for 15 minutes
     fi
     sleep 10
 done
